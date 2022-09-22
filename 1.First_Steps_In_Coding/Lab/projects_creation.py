6c8b73c9904a4f778a05ae17070da87dc8421b0c
tomamishev <47216444+tomamishev@users.noreply.github.com>
2022-09-22 21:09:23 +0300

    Update projects_creation.py

--- 1.First_Steps_In_Coding/Lab/projects_creation.py
+++ 1.First_Steps_In_Coding/Lab/projects_creation.py
@@ -1,3 +1,16 @@
+# Write a program that calculates how many hours it will take an architect to prepare the projects on several construction sites. 
+# The preparation of a project takes three hours.
+
+# INPUT
+# From the console read 2 lines:
+# Architect's name - text
+# Number of projects to draw up - integer in the interval [0 ... 100]
+
+# OUTPUT
+# The console prints:
+# "The architect {architect name} will need {required hours} hours to complete {number of projects} project/s."
+
+
 name = input()
 number_of_projects = int(input())
 time_for_projects = number_of_projects * 3
