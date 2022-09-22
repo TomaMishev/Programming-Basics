a0c651b5ccf1500c2e86ba694a6005842da421eb
tomamishev <47216444+tomamishev@users.noreply.github.com>
2022-09-22 21:03:22 +0300

    Update rectangle_area.py

--- 1.First_Steps_In_Coding/Lab/rectangle_area.py
+++ 1.First_Steps_In_Coding/Lab/rectangle_area.py
@@ -1,4 +1,8 @@
+# Write a console program that enters two integers (the sides of rectangle a and b) and calculates the area of a rectangle with these sides.
+
 side_a = int(input())
 side_b = int(input())
+
 area = side_a * side_b
+
 print(area)
