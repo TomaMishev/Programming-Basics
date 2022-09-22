7b3ec17ecc48f7906c9f66e39161cc8026609193
tomamishev <47216444+tomamishev@users.noreply.github.com>
2022-09-22 21:06:17 +0300

    Update greeting_by_name.py

--- 1.First_Steps_In_Coding/Lab/greeting_by_name.py
+++ 1.First_Steps_In_Coding/Lab/greeting_by_name.py
@@ -1,3 +1,5 @@
+# Write a program that reads text from the console (a person's name) and prints "Hello, <name>!" where <name> is the name entered from the console.
+
 name = input()
 output = f"Hello, {name}!"
 print(output)
