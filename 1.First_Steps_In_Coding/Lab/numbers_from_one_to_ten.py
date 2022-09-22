ea027098f401a8d7406cbb36f4b8c8aab21fe37c
tomamishev <47216444+tomamishev@users.noreply.github.com>
2022-09-22 21:01:31 +0300

    Update and rename numbars_from_one_to_ten.py to numbers_from_one_to_ten.py

---
+++ 1.First_Steps_In_Coding/Lab/numbers_from_one_to_ten.py
@@ -0,0 +1,12 @@
+# Write a Python console program that prints numbers 1 through 10 on individual console lines
+
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+print(6)
+print(7)
+print(8)
+print(9)
+print(10)
