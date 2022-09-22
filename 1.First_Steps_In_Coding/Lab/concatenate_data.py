05b21e82295a56a69ebe23eecce303e55e81a798
tomamishev <47216444+tomamishev@users.noreply.github.com>
2022-09-22 21:07:39 +0300

    Update concatenate_data.py

--- 1.First_Steps_In_Coding/Lab/concatenate_data.py
+++ 1.First_Steps_In_Coding/Lab/concatenate_data.py
@@ -1,3 +1,7 @@
+# Write a program that reads from the console: name, surname, age and city and then prints the following message:
+# "You are <firstName> <lastName>, a <age>-years old person from <town>."
+
+
 first_name = input()
 second_name = input()
 age = int(input())
